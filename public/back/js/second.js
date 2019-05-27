6ebe91828fcc712157094d4f9964daf5a3a19654
Janu <641578178@qq.com>
2019-05-28 03:57:07 +0800

    二级分类完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -65,10 +65,20 @@ $(function() {
           // 获取选中的id
           var id = $(this).data('id');
           $('[name="categoryId"]').val(id);
+
+          // 将隐藏域校验状态,设置成校验成功状态 updateStatus
+          // updateStatus(字段名,校验状态,姜堰规则)
+          $('#form').data('bootstrapValidator').updateStatus("categoryId","VALID");
         })
         
 
         // 4. 进行文件上传初始化
+        /**
+         * 文件上传思路整理:
+         *  1.引包
+         *  2.准备结构,name,data-url
+         *  3.进行文件上传初始化,配置done回调函数
+         */
         $('#fileUpload').fileupload({
           // 配置返回的数据格式
           dataType: "json",
@@ -80,16 +90,85 @@ $(function() {
             $('#imgBox img').attr('src',imgUrl);
             // 将图片地址设置给input
             $('[name="brandLogo"]').val(imgUrl);
+
+            // 手动重置隐藏域的校验状态
+            $('#form').data("bootstrapValidator").updateStatus("brandLogo","VALID");
           }
         })
 
+        // 5. 实现表单校验
+        $('#form').bootstrapValidator({
+          // 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+          // 我们需要对隐藏域进行校验,所以不需要将隐藏域排除到校验范围外
+          excluded: [],
+          //配置校验图标
+          feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', // 校验成功
+            invalid: 'glyphicon glyphicon-remove', // 校验失败
+            validating: 'glyphicon glyphicon-refresh' // 校验中
+          },
+          // 配置字段
+          fields: {
+            // categoryId 分类id
+            categoryId: {
+              validators: {
+                notEmpty: {
+                  message: "请选择一级分类"
+                }
+              }
+            },
 
-        /**
-         * 文件上传思路整理:
-         *  1.引包
-         *  2.准备结构,name,data-url
-         *  3.进行文件上传初始化,配置done回调函数
-         */
+            // brandName 二级分类名称
+            brandName: {
+              validators: {
+                notEmpty: {
+                  message: "请输入二级分类"
+                }
+              }
+            },
+            // nramdLogo  图片地址
+            brandLogo: {
+              validators: {
+                notEmpty: {
+                  message: "请选择图片"
+                }
+              }
+            }
+          }
+
+        })
+
+
+        // 6. 注册表单校验成功事件,阻止默认提交,通过ajax提交
+        $('#form').on("success.form.bv",function(e) {
+          e.preventDefault();
+
+          // 通过ajax提交
+          $.ajax({
+            type: "post",
+            url: "/category/addSecondCategory",
+            data: $('#form').serialize(),
+            dataType: "json",
+            success: function(info) {
+              console.log(info);
+              if(info.success) {
+                // 1. 关闭模态框
+                $('#addModal').modal('hide');
+                // 2. 重新渲染第一页
+                currentPage = 1;
+                render();
+                // 3. 重置模态框的表单,不仅校验状态要重置,文本内容也要重置
+                $('#form').data("bootstrapValidator").resetForm(true);
+                // 4. 手动重置文本内容和图片路径
+                $('#dropdownText').text("请选择一级分类");
+                $('#imgBox img').attr('src',"images/none.png");
+              }
+            }
+          })
+        })
+
+
+ 
 
 
       }
