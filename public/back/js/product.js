7797d1af03873e102ae22975d591c8259a47cf87
Janu <641578178@qq.com>
2019-05-28 20:38:09 +0800

    商品列表页完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -4,6 +4,10 @@
 $(function() {
   var currentPage = 1;
   var pageSize = 5;
+
+  // 定义用来存储已上传图片的数组
+  var picArr = [];
+
   // 1. 一进入页面,发送ajax请求,请求商品数据,通过模板引擎渲染
   render();
   function render() {
@@ -145,6 +149,15 @@ $(function() {
         validators: {
           notEmpty: {
             message: "请输入商品库存"
+          },
+          // 除了非空之外,还必须是非零开头的数字
+          regexp: {
+            // +: 表示出现一次或者多次
+            // \d: 表示数字 0-9
+            // *: 表示出现0次或多次
+            // ?: 表示出现0次或者1次
+            regexp: /^[1-9]\d*$/,
+            message: '商品库存必须是非零开头的数字'
           }
         }
       },
@@ -153,9 +166,10 @@ $(function() {
           notEmpty: {
             message: "请输入商品尺码"
           },
+          // 尺码,还必须是 xx-xx 的格式,x为数字
           regexp: {
-            regexp: /^[a-zA-Z0-9_\.]+$/,
-            message: '尺码格式, 必须是 32-40'
+            regexp: /^\d{2}-\d{2}$/,
+            message: '尺码必须是xx-xx的格式, 例如32-40'
           }
         }
       },
@@ -172,21 +186,85 @@ $(function() {
             message: "请输入商品现价"
           }
         }
+      },
+      // 图片校验
+      picStatus: {
+        validators: {
+          notEmpty: {
+            message: "请选择三张图片"
+          }
+        }
       }
     }
   })
 
   // 5. 文件上传
+  // 多文件上传时,插件会遍历选中的图片,发送多次请求到服务器,将来响应多次
+  // 每次响应,都会调用一次 done 方法
   $('#uploadImg').fileupload({
     // 配置返回数据格式
     dataType: "json",
     // 图片上传完成后,会调用done回调函数
     done: function(e,data) {
-      // 获取上传得到的图片地址
-      var imgUrl = data.result.picAddr;
-      console.log(imgUrl);
-      $('#imgBox img').attr('src',imgUrl)
+      // data.result 是后台响应的内容
+      // 往数组的最前面追加图片对象
+      picArr.unshift(data.result);
+
+      // 往 imgBox 最前面追加 img 元素
+      $('#imgBox').prepend('<img src="'+ data.result.picAddr +'" width="100">');
+
+      // 通过判断数组长度,如果数组长度大于3,将数组最后一项移除
+      if(picArr.length > 3) {
+
+        // 移除数组的最后一项
+        picArr.pop();
+
+        // 移除imgBox中图片结构的最后一项
+        // $('#imgBox img').eq(-1)
+        $('#imgBox img:last-of-type').remove();
+      }
+
+      // 如果处理后图片数组的长度为3,那么就通过校验,手动将picStatus置成VALID
+      if(picArr.length === 3) {
+        $('#form').data("bootstrapValidator").updateStatus("picStatus","VALID");
+      }
     }
+  })
+
+
+  // 注册表单校验成功事件,阻止默认提交,通过ajax进行提交
+  $('#form').on("success.form.bv",function(e) {
+    e.preventDefault();
+    // 获取的是表单元素的数据
+    var paramsStr = $('#form').serialize();
+    // 还需要拼接上图片的数据
+    paramsStr += "&picName1=" + picArr[0].picName + "&picAddr1=" + picArr[0].picAddr;
+    paramsStr += "&picName2=" + picArr[1].picName + "&picAddr2=" + picArr[1].picAddr;
+    paramsStr += "&picName3=" + picArr[2].picName + "&picAddr3=" + picArr[2].picAddr;
+
+    $.ajax({
+      type: "post",
+      url: "/product/addProduct",
+      data: paramsStr,
+      dataType: "json",
+      success: function(info) {
+        console.log(info);
+        if(info.success) {
+          // 添加成功
+          
+          // 关闭模态框
+          $('#addModal').modal('hide');
+          // 页面重新渲染
+          currentPage = 1;
+          render();
+          // 重置表单内容以及校验状态
+          $('#form').data('bootstrapValidator').resetForm(true);
+          // 下拉列表和图片不是表单元素,需要手动重置
+          $('#dropdownText').text("请选择二级分类");
+          $('#imgBox img').remove();
+        }
+      }
+    })
   })
 
 
